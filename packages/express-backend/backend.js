9b7489c96ab5fd4c2b55a5e7bf9feba93c25554b
Devang Bhatnagar <gamefun7102@gmail.com>
2023-11-03 09:53:42 -0700

    Some chess setup

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,13 +4,50 @@ import cors from "cors";
 const app = express();
 const port = 8000;
 
+
+class Pawn {
+  constructor(color, position) {
+    this.color = color; 
+    this.position = position;
+  }
+}
+
+
+class Position{
+	constructor(x, y) {
+	    this.x = x;
+	    this.y = y;
+  }
+}
+
+const pieces = [
+	new Pawn("white", new Position(0,1)),
+	new Pawn("white", new Position(1,1)),
+	new Pawn("white", new Position(2,1)),
+	new Pawn("white", new Position(3,1)),
+	new Pawn("white", new Position(4,1)),
+	new Pawn("white", new Position(5,1)),
+	new Pawn("white", new Position(6,1)),
+	new Pawn("white", new Position(7,1)),
+	new Pawn("black", new Position(0,1)),
+	new Pawn("black", new Position(1,1)),
+	new Pawn("black", new Position(2,1)),
+	new Pawn("black", new Position(3,1)),
+	new Pawn("black", new Position(4,1)),
+	new Pawn("black", new Position(5,1)),
+	new Pawn("black", new Position(6,1)),
+	new Pawn("black", new Position(7,1))
+]
+
+
 app.use(cors());
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+
 });
+
 
 app.listen(port, () => {
     console.log(`Local Chess listening at http://localhost:${port}`);
