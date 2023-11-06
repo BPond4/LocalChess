9a39e41b7095f3fd59a92b114cfe70efdbbeb8c1
NickPerlich <nicholasperlich2003@gmail.com>
2023-11-06 09:24:51 -0800

    added mongodb

---
+++ packages/express-backend/game-service.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import gameModel from "./game.js";
+
+mongoose.set("debug", true);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/users", { useUnifiedTopology: true, })
+  .catch((error) => console.log(error));
+
