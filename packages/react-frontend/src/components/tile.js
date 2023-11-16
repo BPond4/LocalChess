cecc1c1a7b7e35b8ff991329d3f70f78b9846c3c
BPond4 <bpond@calpoly.edu>
2023-11-16 09:22:08 -0800

    working chess

--- packages/react-frontend/src/components/tile.js
+++ packages/react-frontend/src/components/tile.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 
-function Tile({ column, row, chessPiece, isDark, showRowLabel, showColLabel }) {
+function Tile({ column, row, chessPiece, isDark, showRowLabel, showColLabel, onClick, hasPiece, isSelected }) {
   // Determine the class name based on whether it's a dark or light square
   const squareClass = isDark ? "dark-square" : "light-square";
+  
+  const handleClick = () => {
+    onClick(column, row);
+  };
 
   return (
-    <div className={`square ${squareClass}`}>
+    <div className={`square ${squareClass}`} onClick={handleClick}>
       {showRowLabel && <div className="left-center">{row}</div>}
       {showColLabel && <div className="bottom-center">{column}</div>}
       <div className="chess-piece">{chessPiece && <img src={chessPiece} width="90px" height="90px" alt="piece" />}</div>
