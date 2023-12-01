cb7fe4f85b6f2c527bdb07201b6d46bfa5ac11b9
NickPerlich <nicholasperlich2003@gmail.com>
2023-12-01 09:27:28 -0800

    moved start

--- packages/react-frontend/src/components/board.js
+++ packages/react-frontend/src/components/board.js
@@ -213,92 +213,7 @@ export default function Board() {
   //   });
   // };
 
-  function start() {
-    let tempGrid = [];
-    for (let i = rows.length - 1; i >= 0; i--) {
-      for (let j = 0; j < columns.length; j++) {
-        const isDark = (i + j + 2) % 2 === 0;
-        const rowLabel = rows[i];
-        const colLabel = columns[j];
-        const showRowLabel = j === 0;
-        const showColLabel = i === 0;
-        let hasPiece = false;
-
-        let chessPiece = null;
-
-        if (i === 6) {
-          chessPiece = "/Chess_pdt60.png";
-          hasPiece = true;
-        } else if (i === 1) {
-          chessPiece = "/Chess_plt60.png";
-          hasPiece = true;
-        } else if (
-          (i === 0 && j === 0) ||
-          (i === 0 && j === 7)
-        ) {
-          chessPiece = "/Chess_rlt60.png";
-          hasPiece = true;
-        } else if (
-          (i === 7 && j === 7) ||
-          (i === 7 && j === 0)
-        ) {
-          chessPiece = "/Chess_rdt60.png";
-          hasPiece = true;
-        } else if (
-          (i === 0 && j === 1) ||
-          (i === 0 && j === 6)
-        ) {
-          chessPiece = "/Chess_nlt60.png";
-          hasPiece = true;
-        } else if (
-          (i === 7 && j === 6) ||
-          (i === 7 && j === 1)
-        ) {
-          chessPiece = "/Chess_ndt60.png";
-          hasPiece = true;
-        } else if (
-          (i === 0 && j === 2) ||
-          (i === 0 && j === 5)
-        ) {
-          chessPiece = "/Chess_blt60.png";
-          hasPiece = true;
-        } else if (
-          (i === 7 && j === 5) ||
-          (i === 7 && j === 2)
-        ) {
-          chessPiece = "/Chess_bdt60.png";
-          hasPiece = true;
-        } else if (i === 0 && j === 3) {
-          chessPiece = "/Chess_qlt60.png";
-          hasPiece = true;
-        } else if (i === 7 && j === 3) {
-          chessPiece = "/Chess_qdt60.png";
-          hasPiece = true;
-        } else if (i === 0 && j === 4) {
-          chessPiece = "/Chess_klt60.png";
-          hasPiece = true;
-        } else if (i === 7 && j === 4) {
-          chessPiece = "/Chess_kdt60.png";
-          hasPiece = true;
-        }
-
-        tempGrid.push({
-          key: `${colLabel}${rowLabel}`,
-          column: colLabel,
-          row: rowLabel,
-          chessPiece: chessPiece,
-          isDark: isDark,
-          showRowLabel: showRowLabel,
-          showColLabel: showColLabel,
-          onClick: handleSquareClick,
-          hasPiece: hasPiece,
-          isSelected: false
-        });
-      }
-    }
-
-    setGrid(tempGrid);
-  }
+  
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
@@ -306,12 +221,12 @@ export default function Board() {
       .then((response) => response.json())
       .then((result) => {
         console.log(result.message);
-        start();
+        setGrid(start());
       })
       .catch((error) => {
         console.log("Start Error");
       });
-  }, [start]);
+  }, []);
 
   return (
     <div>
@@ -323,4 +238,91 @@ export default function Board() {
       <button onClick={handleFlipBoard}>Flip Board</button>
     </div>
   );
+}
+
+function start() {
+  let tempGrid = [];
+  for (let i = rows.length - 1; i >= 0; i--) {
+    for (let j = 0; j < columns.length; j++) {
+      const isDark = (i + j + 2) % 2 === 0;
+      const rowLabel = rows[i];
+      const colLabel = columns[j];
+      const showRowLabel = j === 0;
+      const showColLabel = i === 0;
+      let hasPiece = false;
+
+      let chessPiece = null;
+
+      if (i === 6) {
+        chessPiece = "/Chess_pdt60.png";
+        hasPiece = true;
+      } else if (i === 1) {
+        chessPiece = "/Chess_plt60.png";
+        hasPiece = true;
+      } else if (
+        (i === 0 && j === 0) ||
+        (i === 0 && j === 7)
+      ) {
+        chessPiece = "/Chess_rlt60.png";
+        hasPiece = true;
+      } else if (
+        (i === 7 && j === 7) ||
+        (i === 7 && j === 0)
+      ) {
+        chessPiece = "/Chess_rdt60.png";
+        hasPiece = true;
+      } else if (
+        (i === 0 && j === 1) ||
+        (i === 0 && j === 6)
+      ) {
+        chessPiece = "/Chess_nlt60.png";
+        hasPiece = true;
+      } else if (
+        (i === 7 && j === 6) ||
+        (i === 7 && j === 1)
+      ) {
+        chessPiece = "/Chess_ndt60.png";
+        hasPiece = true;
+      } else if (
+        (i === 0 && j === 2) ||
+        (i === 0 && j === 5)
+      ) {
+        chessPiece = "/Chess_blt60.png";
+        hasPiece = true;
+      } else if (
+        (i === 7 && j === 5) ||
+        (i === 7 && j === 2)
+      ) {
+        chessPiece = "/Chess_bdt60.png";
+        hasPiece = true;
+      } else if (i === 0 && j === 3) {
+        chessPiece = "/Chess_qlt60.png";
+        hasPiece = true;
+      } else if (i === 7 && j === 3) {
+        chessPiece = "/Chess_qdt60.png";
+        hasPiece = true;
+      } else if (i === 0 && j === 4) {
+        chessPiece = "/Chess_klt60.png";
+        hasPiece = true;
+      } else if (i === 7 && j === 4) {
+        chessPiece = "/Chess_kdt60.png";
+        hasPiece = true;
+      }
+
+      tempGrid.push({
+        key: `${colLabel}${rowLabel}`,
+        column: colLabel,
+        row: rowLabel,
+        chessPiece: chessPiece,
+        isDark: isDark,
+        showRowLabel: showRowLabel,
+        showColLabel: showColLabel,
+        onClick: handleSquareClick,
+        hasPiece: hasPiece,
+        isSelected: false
+      });
+    }
+  }
+
+  return tempGrid;
 }
